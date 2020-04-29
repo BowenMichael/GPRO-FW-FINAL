962b0227e02084a6ae3fe08bf8a6fec285fafd65
MitkoIvanov <mitko4434@gmail.com>
2020-04-29 17:54:28 -0400

    added functions getInput and validInput to GPRO-Input.cpp/GPRO-Input.h

---
+++ GPRO-FW-FINAL/include/GPRO-FW-FINAL/GPRO-Input.h
@@ -0,0 +1,5 @@
+#pragma once
+
+int getInput();
+
+bool validInput(int num, int boardDim); //checks if number fits in the dimentions of the board
--- GPRO-FW-FINAL/source/GPRO-FW-FINAL/GPRO-Input.cpp
+++ GPRO-FW-FINAL/source/GPRO-FW-FINAL/GPRO-Input.cpp
@@ -1,1 +1,21 @@
 #include "GPRO-FW-FINAL/GPRO-Input.h"
+#include <iostream>
+using namespace std;
+
+
+int getInput() {
+	int userInput;
+
+	cout << "Input: ";
+	cin >> userInput;
+
+	return userInput;
+}
+
+bool validInput(int num, int boardDim) {
+	if (num > 0 && num < boardDim) {
+		return true;
+	}
+	
+	return false;
+}
